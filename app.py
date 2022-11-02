9d7043db35f9bc5fb437d3151b128f2feff32b39
Efe Akaröz <69296379+efeakaroz13@users.noreply.github.com>
2022-11-02 23:22:50 +0300

    spesific wikipedia information and translation
    Also I added image scraping from google

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ import requests
 import json
 #from googletrans import Translator
 import translators as ts
-
+import wikipedia
+from bs4 import BeautifulSoup 
 
 app = Flask(__name__)
 @app.route("/")
@@ -13,8 +14,35 @@ def home():
     out = json.loads(requests.get("https://random-word-api.herokuapp.com/word").content)
     out = out[0]
     meaning = ts.google(out,from_language='en', to_language='tr')
-    return {"word":out,"meaning":meaning}
-    return render_template("index.html")
+    try:
+        wikipedia.set_lang("en")
+        summary = wikipedia.summary(wikipedia.search(out)[0],auto_suggest=False) 
+    
+    except wikipedia.DisambiguationError as e1:
+        summary = wikipedia.summary(e1.options[0])
+        
+    except Exception as e:
+        print(e)
+        summary = ""
+
+    summarytr= ts.google(summary,from_language="en",to_language="tr")
+    page = requests.get("https://www.google.com/search?q={}&source=lnms&tbm=isch&sa=X".format(out.replace(" ","+")))
+    soup = BeautifulSoup(page.content,"html.parser")
+    allimages = soup.find_all("img")
+    imgout = []
+    for l in allimages:
+        src= l.get("src")
+        try:
+            src.split("https")[1]
+            imgout.append(src)
+        except:
+            pass
+
+    data =  {"word":out,"meaning":meaning,"summary":summary,"images":imgout,"summarytr":summarytr}
+    return data
+
+
+    #return render_template("index.html")
 
 
 app.run(debug=True)
